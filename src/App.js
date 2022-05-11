9b648dc0a90f2618e0a5897d70ec878c21d64a3c
lalathakur0101 <gumnamibaba0101@gmail.com>
2022-05-11 19:32:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Home,Shows,Reviews} from "./components/index";
-import "./app.css";
 const app = () => {
   return (
-      <div>
+      <>
  <Home/>
  <Shows/>
  <Reviews/>
- </div>
+ </>
   )
 }
 
